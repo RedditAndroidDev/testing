b8170c265aec16d07380f8fa7b59512ab3d9a254
Jared Wilkerson <wilkerson.jared@gmail.com>
2012-03-28 08:04:44 -0400

    Adding test project for setting up Jenkins builds

---
+++ src/com/redditandroiddevelopers/testing/TestingActivity.java
@@ -0,0 +1,14 @@
+package com.redditandroiddevelopers.testing;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestingActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	//just testing, again
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
